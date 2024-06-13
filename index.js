c7671ad3e474d87836d006e8ccde109695903b6e
Ankit Singh <52825138+unkitsingh0@users.noreply.github.com>
2024-06-13 15:25:27 +0530

    backend

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const Typo = require("typo-js");
+const dictionary = new Typo("en_US");
+require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 9000;
+//middleware
+app.use(express.json());
+
+function checkSpell(spell) {
+  var is_spelled_correctly = dictionary.check(spell);
+
+  if (!is_spelled_correctly) {
+    let suggestion = dictionary.suggest(spell);
+    return { response: is_spelled_correctly, spelling: spell, suggestion };
+  } else {
+    return { response: is_spelled_correctly, spelling: "", suggestion: "" };
+  }
+}
+
+app.get("/s/:spell", (req, res) => {
+  let spelling = req.params.spell;
+
+  res.send({ status: "ok", data: checkSpell(spelling) });
+});
+
+app.post("/m", (req, res) => {
+  let spellingArray = req.body.spellingArray;
+
+  let resArray = spellingArray.map((spell) => {
+    return checkSpell(spell);
+  });
+
+  res.send(resArray);
+});
+app.listen(PORT, (e) => {
+  console.log(`Server is running on port no ${PORT}`);
+});
